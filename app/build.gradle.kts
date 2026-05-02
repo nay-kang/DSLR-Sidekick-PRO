e55ea11f5477586663e80d5ceb8f27179d750999
Nay.Kang <kunvunk@gmail.com>
2026-05-02 22:11:31 +0800

    add github action

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,13 @@ configure<ApplicationExtension> {
         }
     }
 
+    signingConfigs.create("release") {
+        storeFile=file("signing/release.jks")
+        storePassword = System.getenv("ANDROID_KEYSTORE_PASSWORD")
+        keyAlias = System.getenv("ANDROID_KEY_ALIAS")
+        keyPassword = System.getenv("ANDROID_KEY_PASSWORD")
+    }
+
     buildTypes {
         getByName("debug") {
             applicationIdSuffix = ".debug"
